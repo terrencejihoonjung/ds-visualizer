a9710847795ff832035ed492b3738ce0485c4c35
terrencejihoonjung <terrencejung928@gmail.com>
2024-08-10 12:44:11 -0700

    fix: modified config and hashrouter

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -1,27 +1,18 @@
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
+
 import Home from "./pages/Home";
 import DynamicArray from "./pages/DynamicArray";
 import Layout from "./Layout";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-      {
-        path: "dynamic-array",
-        element: <DynamicArray />,
-      },
-    ],
-  },
-]);
-
 function AppRouter() {
-  return <RouterProvider router={router} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="dynamic-array" element={<DynamicArray />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default AppRouter;
