378f4fa44acd236c8a7c3cc39cf9ad1538222d8b
terrencejihoonjung <terrencejung928@gmail.com>
2024-08-19 12:22:45 -0700

    fix: removed waitFor import

--- src/tests/components/DSPlaygrounds/HashTablePG.test.tsx
+++ src/tests/components/DSPlaygrounds/HashTablePG.test.tsx
@@ -1,4 +1,4 @@
-import { render, screen, within, waitFor } from "@testing-library/react";
+import { render, screen, within } from "@testing-library/react";
 import userEvent from "@testing-library/user-event";
 import { BrowserRouter as Router } from "react-router-dom";
 import HashTablePG from "../../../components/DSPlaygrounds/HashTablePG";
